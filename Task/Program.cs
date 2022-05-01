4a055a460637fcf4dcc08729f9eb3c2d23f3c9dc
MaxHarper <islowget@gmail.com>
2022-05-02 01:50:09 +0300

    #1 : Первая задача

---
+++ Task/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число");
+            int num;
+            while (true)
+            {
+                if(int.TryParse(Console.ReadLine(), out num))
+                {
+                   break;
+                }
+                Console.WriteLine("Ошибка ввода, введите целое число!");
+            }
+
+            Console.WriteLine($"Введенное число - {num}");
+            if (num % 2 == 1)
+            {
+                Console.WriteLine("Число нечетное");
+            }
+            else
+            {
+                Console.WriteLine("Число четное");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

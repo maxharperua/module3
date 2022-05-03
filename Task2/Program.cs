8b320f53b0ed87c616feaa266ba5eb06a765d481
MaxHarper <islowget@gmail.com>
2022-05-04 01:49:19 +0300

    #3 : Задание 3 и 2

---
+++ Task2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Task2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int cardCount = 0;
+            int totalScore = 0;
+            
+            Console.WriteLine("Здраствуйте, введите количество карт");
+            
+            while (true)
+            {
+                if(int.TryParse(Console.ReadLine(), out cardCount))
+                {
+                    break;
+                }
+                Console.WriteLine("Ошибка ввода, введите целое число!");
+            }
+            
+            for (int i = 1; i <= cardCount; i++)
+            {
+                Console.WriteLine("Введите номинал карты номер " + i );
+                string input = Console.ReadLine();
+ 
+                bool result = int.TryParse(input, out var number);
+                if (result && number >= 2 && number <= 10)
+                {
+                    totalScore += number;
+                }
+                else
+                {
+                    switch (input)
+                    {
+                        case "J":
+                        case "j":
+                        case "Q":
+                        case "q":
+                        case "K":
+                        case "k":
+                        case "T":
+                        case "t":
+                            totalScore += 10;
+                            break;
+                    }
+                }
+            }
+            
+            Console.WriteLine("Значение суммы карт - " + totalScore);
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Task3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число для проверкаи на признак простоты");
+            int number;
+            bool isSimple = true;
+            while (true)
+            {
+                if(int.TryParse(Console.ReadLine(), out number) && number >= 2)
+                {
+                    break;
+                }
+                Console.WriteLine("Ошибка ввода, введите целое число! > 1");
+            }
+            
+            int i = 2;
+            while (i <= number / 2)
+            {
+                if (number % i == 0)
+                {
+                    isSimple = false;
+                    break;
+                }
+                i++;
+            }
+            
+            Console.WriteLine(isSimple ? "Число простое" : "Число не простое");
+            Console.ReadKey();
+            
+        }
+        
+    }
+}
